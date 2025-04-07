6dcf784dccfe87678553d5116be6b52eab5f62c3
Gross <slovenskicoder@gmail.com>
2025-04-07 08:03:29 +0300

    zero adam moments

--- tests/test_roberta_xgblora.py
+++ tests/test_roberta_xgblora.py
@@ -42,21 +42,27 @@ def main():
     model_name = "FacebookAI/xlm-roberta-large"
 
     # task = 'sst2'
-    task = 'qnli'
+    task = 'mnli'
     dataset = load_dataset("glue", task)
 
     # Загружаем токенизатор
     tokenizer = XLMRobertaTokenizer.from_pretrained(model_name)
 
-    train_dataset = create_glue_dataset(dataset["train"], task, tokenizer)
-    eval_dataset = create_glue_dataset(dataset["validation"], task, tokenizer)
+    MAX_LENGTH = 512
+    tokenized_datasets = create_glue_dataset(dataset, task, tokenizer, max_length=MAX_LENGTH)
+    train_dataset = tokenized_datasets["train"]
+    eval_datasets = [
+        # tokenized_datasets["validation"],
+        tokenized_datasets["validation_matched"],
+        tokenized_datasets["validation_mismatched"],
+    ]
 
     # train_loader = DataLoader([train_dataset[i] for i in range(64)], batch_size=64, shuffle=True)
-    train_loader = DataLoader(train_dataset, batch_size=64)
-    eval_loader = DataLoader(eval_dataset, batch_size=128)
+    train_loader = DataLoader(train_dataset, batch_size=48, shuffle=True)
+    eval_loaders = [DataLoader(eval_dataset, batch_size=64) for eval_dataset in eval_datasets]
 
     # Шаг 1: Загружаем "базовую" модель
-    NUM_CLASSES = 2
+    NUM_CLASSES = 3
     model = XLMRobertaForSequenceClassification.from_pretrained(model_name, num_labels=NUM_CLASSES)
 
     # Оборачиваем модель в Pseudo-модель
@@ -86,7 +92,7 @@ def main():
         ],
     )
 
-    weights_storage.build_storage(1, 4, torch.float32, 0.1)
+    weights_storage.build_storage(1, 2, torch.float32, 0.1)
     model.to(device)
 
     for param in model.parameters():
@@ -114,7 +120,7 @@ def main():
     ]
     optimizer = optim.AdamW([
         {"params": base_params, "lr": 2e-5, "weight_decay": 0.01},
-        {"params": lora_params, "lr": 4e-5, "weight_decay": 0.01},
+        {"params": lora_params, "lr": 5e-5, "weight_decay": 0.01},
     ])
 
     scaler = torch.cuda.amp.GradScaler()
@@ -125,9 +131,9 @@ def main():
     weights_storage.reset_lora()
     train_epochs = 3
     steps_per_epoch = len(train_loader)
-    k = 20
+    k = 10 * 3 * 3
     update_freq = train_epochs * steps_per_epoch // k
-    print(f'[+] udpate frequency is {update_freq}')
+    print(f'[+] update frequency is {update_freq}')
 
     num_epochs = train_epochs  # для примера поменьше эпох
     step_index = 0
@@ -176,7 +182,7 @@ def main():
             predictions = torch.argmax(outputs.logits, dim=-1)
             train_accuracy_metric.update(predictions, batch["labels"])
 
-            if (step + 1) % 50 == 0:
+            if (step + 1) % 100 == 0:
                 avg_loss = running_loss / (step + 1)
                 current_acc = train_accuracy_metric.compute().item()
                 print(f"Epoch {epoch + 1}, Step {step + 1}/{len(train_loader)}, "
@@ -184,8 +190,16 @@ def main():
                 print(weights_storage.enabled_modules_count)
 
             if (step_index + 1) % update_freq == 0:
+                print('[*] udpating xgblora adapters')
                 weights_storage.apply_weights()
                 weights_storage.reset_lora()
+                for param in lora_params:
+                    state = optimizer.state.get(param, None)
+                    if state is not None:
+                        if "exp_avg" in state:
+                            state["exp_avg"].zero_()
+                        if "exp_avg_sq" in state:
+                            state["exp_avg_sq"].zero_()
 
             step_index += 1
 
@@ -195,21 +209,23 @@ def main():
 
         # Оценка на валидационном наборе
         model.eval()
-        eval_loss = 0.0
-        eval_accuracy_metric.reset()
-        with torch.no_grad():
-            for batch in eval_loader:
-                batch = {k: v.to(device) for k, v in batch.items()}
-                with torch.cuda.amp.autocast():
-                    outputs = model(**batch)
-                    loss = outputs.loss
-                eval_loss += loss.item()
-                predictions = torch.argmax(outputs.logits, dim=-1)
-                eval_accuracy_metric.update(predictions, batch["labels"])
+        for test_idx in range(len(eval_datasets)):
+            eval_loader = eval_loaders[test_idx]
+            eval_loss = 0.0
+            eval_accuracy_metric.reset()
+            with torch.no_grad():
+                for batch in eval_loader:
+                    batch = {k: v.to(device) for k, v in batch.items()}
+                    with torch.cuda.amp.autocast():
+                        outputs = model(**batch)
+                        loss = outputs.loss
+                    eval_loss += loss.item()
+                    predictions = torch.argmax(outputs.logits, dim=-1)
+                    eval_accuracy_metric.update(predictions, batch["labels"])
 
-        avg_eval_loss = eval_loss / len(eval_loader)
-        eval_acc = eval_accuracy_metric.compute().item()
-        print(f"[Epoch {epoch + 1}] eval loss: {avg_eval_loss:.4f}, eval accuracy: {eval_acc:.4f}")
+            avg_eval_loss = eval_loss / len(eval_loader)
+            eval_acc = eval_accuracy_metric.compute().item()
+            print(f"[Epoch {epoch + 1}] eval-{test_idx} loss: {avg_eval_loss:.4f}, eval accuracy: {eval_acc:.4f}")
 
 
 if __name__ == "__main__":
